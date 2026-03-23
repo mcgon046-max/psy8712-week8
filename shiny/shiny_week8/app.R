ec1eae3d0a76b12a5b04d84a61c3e30d7735f01c
Patrick Mcgonagle <mcgon046@umn.edu>
2026-03-23 02:22:55 -0500

    Finished

--- shiny/shiny_week8/app.R
+++ shiny/shiny_week8/app.R
@@ -14,44 +14,67 @@ library(dplyr)
 library(readr)
 
 # Dataset 
-shiny_data <- read_csv("shiny/shiny_week8/shiny_data.csv")
+shiny_data <- read_csv("shiny_data.csv")
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Define application title because it helps users identify the app's purpose.
+  titlePanel("Scatterplot App"),
+  
+  # Side bar creation
+  sidebarLayout(
+    
+    sidebarPanel(
+      # Gender drop down menu 
+      selectInput("gender", "Select Gender:", choices = c("All", "Male", "Female"), selected = "All"),
+      
+      # Error band drop down 
+      selectInput("errorBand", "Error Band:", choices = c("Display Error Band", "Suppress Error Band"), selected = "Display Error Band"),
+      
+      # Date Exclusion 
+      selectInput("dates", "Participants before July 1, 2017:", choices = c("Include", "Exclude"), selected = "Include")
+    ),
+    
+    mainPanel(
+      # Scatterplot 
+      plotOutput("scatterPlot")
     )
+  )
 )
+  
+     
 
-# Define server logic required to draw a histogram
+# Server logic for scatter plot 
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$scatterPlot <- renderPlot({
+    
+    # Local copy of data 
+    shiny_dat <- shiny_data
+    
+    # gender selection logic 
+    if (input$gender != "All") {
+      shiny_dat <- filter(shiny_dat, gender == input$gender)
+    }
+    
+    # Exclusion based on date logic
+    if (input$dates == "Exclude") {
+      # Exact date range
+     shiny_dat <- filter(shiny_dat, timeStart >= as.Date("2017-07-01"))
+    }
+    
+    # Error band logic 
+    show_band <- input$errorBand == "Display Error Band"
+    
+    # Plot from markdown file 
+    ggplot(shiny_dat, aes(x = m_q1_q6, y = m_q8_q10)) +
+      geom_jitter(width = 0.2, height = 0.2) +
+      geom_smooth(method = "lm", color = "purple", se = show_band) +
+      labs(title = "Relationship Between Mean Scores of Q1-Q6 and Q8-Q10",
+           x = "Means of Questions 1-6",
+           y = "Means of Questions 8-10")
+  })
 }
 
 # Run the application 
